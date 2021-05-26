cfc49e3b91bbb131e79f7ad32cc811230f85538a
jagruti-perpetualny <jagruti@perpetualny.com>
2021-05-26 12:52:49 +0530

    Created first version of the design system, with , , ,  and  components.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 export * from "./stories/Button";
-export * from "./stories/Button.stories";
 export * from "./stories/Header";
-export * from "./stories/Header.stories";
 export * from "./stories/Page";
-export * from "./stories/Page.stories";
