7cb5fd6689c00e1b310c99803021f4b8a816ab92
jagruti-perpetualny <jagruti@perpetualny.com>
2021-05-26 15:08:18 +0530

    css files imported in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import * as btncss from "../src/stories/button.css";
+import * as headercss from "../src/stories/header.css";
+export { btncss, headercss };
+
 export * from "./stories/Button";
 export * from "./stories/Header";
 export * from "./stories/Page";
